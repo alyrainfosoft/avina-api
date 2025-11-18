560574fa5800ad79a2524b4fa3772b70679bc445
Rakesh <ugharejarakesh5566@gmail.com>
2025-11-18 23:05:46 +0530

    stage

--- src/version-4/routes/admin/s3Presign.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getPresignedUrlFn } from "../../controllers/masters/master.controller";
-import { publicAuthentication } from "../../../middlewares/authenticate";
-
-export default (app: Router) => {
-
-      app.post("/presign", [publicAuthentication], getPresignedUrlFn);
-      
-  };
